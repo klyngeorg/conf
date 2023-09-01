c7cdc0897ec20903d2f1792c6a8d03c07a07b3f7
Simen A. W. Olsen <so@bjerk.io>
2023-09-01 22:02:58 +0200

    chore: set projectId

--- projects/eventorg/src/providers/gcp.ts
+++ projects/eventorg/src/providers/gcp.ts
@@ -1,3 +1,6 @@
+import { project } from '../project';
 import * as gcp from '@pulumi/gcp';
 
-export const provider = new gcp.Provider('gcp');
+export const provider = new gcp.Provider('gcp', {
+  project: project.id,
+});
