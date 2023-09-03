a9c00cf6acd12d1b08cbed95eee68b6650548fe9
Simen A. W. Olsen <so@bjerk.io>
2023-09-03 22:25:39 +0200

    add container setup

--- projects/eventorg/src/artifact-registry.ts
+++ projects/eventorg/src/artifact-registry.ts
@@ -1,8 +1,12 @@
 import * as gcp from '@pulumi/gcp';
-import { region } from './config';
+import * as github from '@pulumi/github';
+import { developers, region } from './config';
 import { project } from './project';
 import { apiServices } from './api-services';
-import { provider } from './providers/gcp';
+import { provider as gcpProvider } from './providers/gcp';
+import { provider as githubProvider } from './providers/github';
+import { serviceAccount } from './service-account';
+import { interpolate } from '@pulumi/pulumi';
 
 export const artifactRepository = new gcp.artifactregistry.Repository(
   'artifact-registry',
@@ -11,5 +15,38 @@ export const artifactRepository = new gcp.artifactregistry.Repository(
     location: region,
     format: 'DOCKER',
   },
-  { provider, dependsOn: apiServices },
+  { provider: gcpProvider, dependsOn: apiServices },
+);
+
+new gcp.artifactregistry.RepositoryIamMember(
+  `eventorg-repo-admin`,
+  {
+    member: serviceAccount.email.apply(email => `serviceAccount:${email}`),
+    role: 'roles/artifactregistry.repoAdmin',
+    repository: artifactRepository.id,
+  },
+  { provider: gcpProvider },
+);
+
+developers.map(
+  member =>
+    new gcp.artifactregistry.RepositoryIamMember(
+      `${member}-repo-reader`,
+      {
+        member,
+        role: 'roles/artifactregistry.reader',
+        repository: artifactRepository.id,
+      },
+      { provider: gcpProvider },
+    ),
+);
+
+new github.ActionsSecret(
+  'container-registry-secret',
+  {
+    repository: 'eventorg',
+    secretName: 'CONTAINER_REGISTRY',
+    plaintextValue: interpolate`${artifactRepository.location}-docker.pkg.dev/${project.projectId}/${artifactRepository.repositoryId}`,
+  },
+  { provider: githubProvider, deleteBeforeReplace: true },
 );
